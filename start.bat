3183a19d6b4410cfd081995d456d7937294c45c6
sky4hell <383486832@qq.com>
2026-07-31 17:06:54 +0800

    docs: 更新修复总结+start.bat改用python.exe

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
 title mangaGo Worker
-start "" cmd /c "cd /d %~dp0 && %~dp0..\manga-image-translator\venv\Scripts\pythonw.exe worker.py"
+start "" "%~dp0..\manga-image-translator\venv\Scripts\python.exe" "%~dp0worker.py"
